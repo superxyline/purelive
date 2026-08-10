9d0a88fc809020c550e9f42250a8e2fc8d8d00fe
superxyline <superxyline@noreply.gitee.com>
2026-08-10 20:56:33 +0800

    chore: 版本号升至 0.1.5.2（构建号 10）

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         multiDexEnabled = true 
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = "0.1.5.1"
+        versionName = "0.1.5.2"
     }
 
     signingConfigs {
